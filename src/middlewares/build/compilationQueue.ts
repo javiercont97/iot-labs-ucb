dd87e61817ed74a478d9f9ac35dc3b4e19c01069
javiercont97 <javierconts3@live.com>
2021-03-30 15:53:21 -0400

    compilation queue with mutexes

--- src/middlewares/build/compilationQueue.ts
+++ src/middlewares/build/compilationQueue.ts
@@ -1,34 +1,41 @@
 import { AMMOUNT_OF_COMPILATION_WORKERS, appLogger, CPUS_PER_WORKER } from "../../config/constants";
 import DockerCompiler from "./wasmCompiler";
-
+import {Mutex} from 'async-mutex';
 
 export class CompilationQueue {
     private static tasks: Array<{path: string, app: string, appID: string, userMail: string, userName: string}> = [];
     private static workers: Array<DockerCompiler> = [];
+    private static mutex: Mutex;
 
     public static init (): void {
         appLogger.verbose('WASK_COMPILER_QUEUE', `Init worker pool for WASM compilation with ${AMMOUNT_OF_COMPILATION_WORKERS} workers`);
+        this.mutex = new Mutex();
+
+
         for (let i = 0 ; i < AMMOUNT_OF_COMPILATION_WORKERS ; i++) {
             let worker = new DockerCompiler(CPUS_PER_WORKER);
             this.workers.push(worker);
             this.tasks = [];
 
             worker.on('jobsdone', (worker: DockerCompiler) => {
-                appLogger.verbose('WASK_COMPILER_QUEUE', 'Job\'s finished');
-                if(!worker.busy) {
-                    if(this.tasks.length > 0) {
-                        appLogger.verbose('WASK_COMPILER_QUEUE', 'Asign job');
-                        let work = this.tasks.pop();
-                        worker.compile(work.path, work.app, work.appID, work.userMail, work.userName);
+                this.mutex.acquire()
+                .then( release => {
+                    appLogger.verbose('WASK_COMPILER_QUEUE', 'Job\'s finished');
+                    if(!worker.busy) {
+                        if(this.tasks.length > 0) {
+                            appLogger.verbose('WASK_COMPILER_QUEUE', 'Asign job');
+                            let work = this.tasks.pop();
+                            worker.compile(work.path, work.app, work.appID, work.userMail, work.userName);
+                        }
                     }
-                }
+                    release();
+                });
             });
         }
     }
 
     private static checkIfJobsAvailable(): void {
         appLogger.verbose('WASK_COMPILER_QUEUE', 'Checking if any job is available');
-
         this.workers.forEach(currentWorker => {
             if(!currentWorker.busy) {
                 if(this.tasks.length > 0) {
@@ -49,14 +56,18 @@ export class CompilationQueue {
      * @param userName Owner name
      */
     public static enqueueJob(path: string, app: string, appID: string, userMail: string, userName: string): void {
-        this.tasks.push({
-            path,
-            app,
-            appID,
-            userMail,
-            userName
+        this.mutex.acquire()
+        .then( release => {
+            this.tasks.push({
+                path,
+                app,
+                appID,
+                userMail,
+                userName
+            });
+    
+            this.checkIfJobsAvailable();
+            release();
         });
-
-        this.checkIfJobsAvailable();
     }
 }
